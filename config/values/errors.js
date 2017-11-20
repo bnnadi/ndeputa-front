666a7bd13a6fb0a4a724a6667e94b7fcafd4f5f8
Bisike Nnadi <bisike@hyrecar.com>
2017-11-19 19:26:58 -0800

    updates error code abrv

--- config/values/errors.js
+++ config/values/errors.js
@@ -1,9 +1,9 @@
 var ERRORS = {};
 
-ERRORS['NRC-00000'] = 'User is not signed in.';
-ERRORS['NRC-00001'] = 'User is already signed in.';
-ERRORS['NRC-00002'] = 'User action not permitted.';
-ERRORS['NRC-00003'] = 'Not supported yet.';
-ERRORS['NRC-00004'] = 'Not found.';
+ERRORS['NN-00000'] = 'User is not signed in.';
+ERRORS['NN-00001'] = 'User is already signed in.';
+ERRORS['NN-00002'] = 'User action not permitted.';
+ERRORS['NN-00003'] = 'Not supported yet.';
+ERRORS['NN-00004'] = 'Not found.';
 
 module.exports = ERRORS;
