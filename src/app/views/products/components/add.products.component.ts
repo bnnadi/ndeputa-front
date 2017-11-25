905fce5f5205f7ce6deb54470383ac5a39158d2f
Bisike Nnadi <bisike@hyrecar.com>
2017-11-24 20:32:04 -0800

    adds image upload/viewer to products

--- src/app/views/products/components/add.products.component.ts
+++ src/app/views/products/components/add.products.component.ts
@@ -9,6 +9,7 @@ import { Company } from 'app/models/company.model';
 })
 export class AddProductsComponent {
   companies: any;
+  url: string;
 
   constructor(ps: ProductService, private cs: CompanyService ) { 
     this.getCompanies();
@@ -16,11 +17,25 @@ export class AddProductsComponent {
 
   getCompanies() {
     this.cs.getCompanies()
-      .subscribe(companies => {this.companies = companies.result.rows});
+      .subscribe(companies => {
+        this.companies = companies.result.rows;
+      });
   }
 
   addProduct(form: any) {
     console.log(form);
+  }
+
+  fileChange(event:any) {
+    if(event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = (event:any) => {
+        this.url = event.target.result;
+      }
+
+      reader.readAsDataURL(event.target.files[0]);
+    }
   }
 
 }
