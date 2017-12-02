2566f819e29444d41f9c7d4b2162d436e57450b7
Bisike Nnadi <bisike@hyrecar.com>
2017-12-01 19:46:33 -0800

    removes server files

--- config/values/errors.js
+++
@@ -1,9 +0,0 @@
-var ERRORS = {};
-
-ERRORS['NN-00000'] = 'User is not signed in.';
-ERRORS['NN-00001'] = 'User is already signed in.';
-ERRORS['NN-00002'] = 'User action not permitted.';
-ERRORS['NN-00003'] = 'Not supported yet.';
-ERRORS['NN-00004'] = 'Not found.';
-
-module.exports = ERRORS;
