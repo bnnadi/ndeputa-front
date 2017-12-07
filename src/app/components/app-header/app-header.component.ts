5da14839c9baa995798cb1d2c90788605fb7d8ad
Bisike Nnadi <bisike@hyrecar.com>
2017-12-06 17:28:12 -0800

    adds user to header dropdown, updates dropdown to ngBootstrap

--- src/app/components/app-header/app-header.component.ts
+++ src/app/components/app-header/app-header.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../_access/auth.service';
+import { UserService } from 'app/services';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { User } from 'app/models';
 
 @Component({
   selector: 'app-header',
   templateUrl: './app-header.component.html'
 })
-export class AppHeaderComponent {
+export class AppHeaderComponent implements OnInit {
   notifyCount: any;
+  user: User;
 
-  constructor(private as: AuthService) {}
+  constructor(private as: AuthService, private us: UserService) {}
+
+  ngOnInit()  {
+    this.user = this.us.getCurrentUser();
+  }
 
   logout () {
     return this.as.logout();
